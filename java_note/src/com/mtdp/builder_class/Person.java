32738dad3e549d56f9db3cb76e9bfef592f3bc08
zhenchai <zhenchai@zhenchaideMacBook-Pro.local>
2017-06-26 19:22:33 +0800

    “使用构建器生成对象”

---
+++ java_note/src/com/mtdp/builder_class/Person.java
@@ -0,0 +1,56 @@
+package com.mtdp.builder_class;
+
+/**
+ * Created by zhenchai on 2017/6/26.
+ */
+public class Person {
+
+    private final String userName;
+    private final int age;
+
+    private final int moneyTotal;
+    private final String address;
+    private final double height;
+
+    public static class Builder {
+        private final String userName;
+        private final int age;
+
+        //optional parameters
+        private int moneyTotal  = 0;
+        private String address = "";
+        private double height = 1.0;
+
+        public Builder(String userName, int age){
+            this.userName = userName;
+            this.age = age;
+        }
+
+        public Builder moneyTotal(int moneyTotal){
+            moneyTotal = moneyTotal;
+            return this;
+        }
+
+        public Builder address(int address){
+            address = address;
+            return this;
+        }
+
+        public Builder height(int height){
+            height = height;
+            return this;
+        }
+
+        public Person build(){
+            return new Person(this);
+        }
+    }
+
+    private Person(Builder builder){
+        userName = builder.userName;
+        age = builder.age;
+        moneyTotal = builder.moneyTotal;
+        address = builder.address;
+        height = builder.height;
+    }
+}
